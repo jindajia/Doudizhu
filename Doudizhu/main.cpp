ab655da2c3572d5a7bfa318bc38e4d350b615a88
Jiajinda <13766785543@163.com>
2020-03-25 16:14:27 +0800

    Initial Commit

---
+++ Doudizhu/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Doudizhu
+//
+//  Created by 贾金达 on 3/25/20.
+//  Copyright © 2020 贾金达. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
