fa5b49cebe03b0f1d7cf6965b584f20a5d0efde8
Jiajinda <13766785543@163.com>
2020-03-25 16:46:12 +0800

    新增了map字典保存牌力

--- Doudizhu/main.cpp
+++ Doudizhu/main.cpp
@@ -2,16 +2,21 @@
 #include<iostream>
 #include<algorithm>
 #include<vector>
+#include<map>
 using namespace std;
 //斗地主
 enum suit{spade,hearts,clubs,diamonds,small,big};
 string suit_str[]={"黑桃","红心","梅花","方片","小","大"};
+map<string,int> map_Power;
 class Poker{
     suit type;//表示牌的花色
     string number;//表示牌的点数
     int power;//表示牌力大小；大王>小王>2>A>K>Q>J>10>......3//大王的牌力是15
 public:
-    Poker(suit t,string n,int p):type(t),number(n),power(p){}
+    Poker(suit t,string n):type(t),number(n),power(map_Power[suit_str[t]+n]){}
+    bool operator<(const Poker& p)const{
+        return power<p.power;
+    }
     bool operator>(const Poker& p)const{
         return power>p.power;
     }
@@ -29,22 +34,36 @@ public:
      vector<Poker> v;
     DeckCards(){
         for(int i=3;i<=10;i++){
+            
             for(int j=0;j<4;++j){
-                v.push_back(Poker((suit)j,to_string(i),i-2));
+                map_Power[suit_str[j]+to_string(i)]=i-2;
+                v.push_back(Poker((suit)j,to_string(i)));
             }
         }
-        for(int j=0;j<4;++j)
-            v.push_back(Poker((suit)j,"J",9));
-        for(int j=0;j<4;++j)
-            v.push_back(Poker((suit)j,"Q",10));
-        for(int j=0;j<4;++j)
-            v.push_back(Poker((suit)j,"K",11));
-        for(int j=0;j<4;++j)
-            v.push_back(Poker((suit)j,"A",12));
-        for(int j=0;j<4;++j)
-            v.push_back(Poker((suit)j,"2",13));
-        v.push_back(Poker((suit)4,"鬼",14));
-        v.push_back(Poker((suit)5,"鬼",15));
+        for(int j=0;j<4;++j){
+            map_Power[suit_str[j]+"J"]=9;
+            v.push_back(Poker((suit)j,"J"));
+        }
+        for(int j=0;j<4;++j){
+            map_Power[suit_str[j]+"Q"]=10;
+            v.push_back(Poker((suit)j,"Q"));
+        }
+        for(int j=0;j<4;++j){
+            map_Power[suit_str[j]+"K"]=11;
+            v.push_back(Poker((suit)j,"K"));
+        }
+        for(int j=0;j<4;++j){
+            map_Power[suit_str[j]+"A"]=12;
+            v.push_back(Poker((suit)j,"A"));
+        }
+        for(int j=0;j<4;++j){
+            map_Power[suit_str[j]+"2"]=13;
+            v.push_back(Poker((suit)j,"2"));
+        }
+        map_Power["小鬼"]=14;
+        v.push_back(Poker((suit)4,"鬼"));
+        map_Power["大鬼"]=15;
+        v.push_back(Poker((suit)5,"鬼"));
         
         
     }
@@ -81,10 +100,15 @@ public:
         else{
             //首先判断牌型
             if(size==1){
-                
+                return v[0].getPower()<p2.v[0].getPower();
             }
             else if(size==2){
-                
+                if(p2.v[0].getPower()==14&&p2.v[1].getPower()==15)
+                    return true;
+                else{
+                    
+                }
+                    
             }
             else if(size==3){
                 
@@ -114,6 +138,13 @@ public:
 int main(){
     DeckCards d;
     d.print();
+    vector<Poker> v1;
+    vector<Poker> v2;
+    v1.push_back(Poker((suit)0,"2"));
+    v2.push_back(Poker((suit)1,"A"));
     cout<<d.v.size();
+    Play p1(v1);
+    Play p2(v2);
+    cout<<(p2<p1);
 }
 
